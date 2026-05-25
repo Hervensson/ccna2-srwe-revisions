ffa67e4945be87a14cfcad39524ad5e1da63b54d
Hervensson <114006512+Hervensson@users.noreply.github.com>
2026-05-25 14:07:54 +0200

    Refresh cache for advanced revision tools

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ccna-srwe-v18";
+const CACHE_NAME = "ccna-srwe-v19";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -7,12 +7,14 @@ const CORE_ASSETS = [
   "./ux-polish.css",
   "./mobile-history.css",
   "./mobile-fix.css",
+  "./revision-pro.css",
   "./questions.js",
   "./app.js",
   "./enhance.js",
   "./launch-fix.js",
   "./home-button.js",
   "./ux-polish.js",
+  "./revision-pro.js",
   "./force-home.js",
   "./site.webmanifest",
   "./icon.svg",
